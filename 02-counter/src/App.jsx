0e07648e52c4e3288b6d8522a3a8646eaaa273b6
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-09-23 18:14:34 +0500

    counter using state add and remove

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -2,18 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  let [counter, setCounter] = useState(15);
 
-let counter = 5;
+  // let counter = 5;
+// Add vaue funtion 
+  const addValue = () => {
 
-  const addValue = () => { console.log("Value added" , Math.random());}
+    // counter = counter + 1
+
+    setCounter(counter + 1);
+//console.log("Value added", counter);
+  }
+
+  // Remve Value funtion 
+  const removeValue = () =>{
+    setCounter(counter - 1);
+
+  }
   return (
     <>
       <h1>Counter with React</h1>
       <h2>Counter Value : {counter}</h2>
       <button
-      onClick={addValue}>Add Value </button>
-       <br/>
-      <button>Remove Value </button>
+        onClick={addValue}>Add Value </button>
+      <br />
+      <button onClick={removeValue}> Remove Value </button>
     </>
   )
 }
